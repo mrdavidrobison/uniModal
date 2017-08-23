98da7738deb1a343bd9109e43cc2043f9e68bdde
david robison <mrdavidrobison@gmail.com>
2017-08-23 15:26:18 -0600

    username and password accepts entries

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
-var modal = document.getElementById('myModal');
+var modal = document.getElementById('modal-wrapper');
 var loginBtn = document.getElementById('myBtn');
 var closeBtn = document.getElementById('close');
+
 loginBtn.onclick = function() {
   modal.style.display = "block";
 }
