74a2afed0e6b5db1830c90d7b64f9820bb6aebeb
david robison <mrdavidrobison@gmail.com>
2017-08-23 09:03:28 -0600

    modal pops up

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,11 @@
 var modal = document.getElementById('myModal');
-
 var loginBtn = document.getElementById('myBtn');
+var closeBtn = document.getElementById('close');
 loginBtn.onclick = function() {
   modal.style.display = "block";
+}
+
+
+closeBtn.onclick = function() {
+  modal.style.display = "none";
 }
